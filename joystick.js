db39407b61b09393d4f8fcc626e2b9479d686bea
Lloyd Beaufils <lloyd99@comcast.net>
2017-12-02 18:22:34 -0500

    Updated phone stuff

--- joystick.js
+++ joystick.js
@@ -6,6 +6,71 @@ function Joystick(centerX, centerY, radius) {
 	this.centerX = centerX;
 	this.centerY = centerY;
 	this.radius = radius;
-	this.tiltPosX = centerX;
-	this.tiltPosY = centerY;
+
+	//this.tiltPosX = centerX;
+	//this.tiltPosY = centerY;
+
+	this.tiltMagnitude = 0;
+	this.tiltAngle = 0;
+
+	this.initialTouchX = 0;
+	this.initialTouchY = 0;
+}
+
+//Handles what happens when the joystick is touched
+function handleTouchStart(event){
+	event.preventDefault();
+
+	this.initialTouchX = touches[0].clientX;
+	this.initialTouchY = touches[0].clientY;
+}
+
+//Move the joystick to the touch
+function handleTouchMove(event){
+	event.preventDefault();
+
+	var touch = touches[0];
+	var touchX = touch.clientX;
+	var touchY = touch.clientY;
+
+	var hypotenuse = touchX * touchX + touchY * touchY;
+
+	//clamp the hypotenuse to the radius
+	//var hypotenuseFixed = max(0, min(radius, hypotenuse)); 
+
+	//this.tiltPosX = this.initialTouchX + ();
+	//this.tiltPosY = ;
+
+	this.tiltMagnitude = max(0, min(radius, hypotenuse));
+	this.tiltAngle = Math.atan2(touchY - this.initialTouchY, touchX - this.initialTouchX);
+}
+
+//On touch end, snap the joystick back to its starting position
+function handleTouchEnd(event){
+	event.preventDefault();
+	//this.tiltPosX = this.centerX;
+	//this.tiltPosY = this.centerY;
+
+	this.tiltMagnitude = 0;
+	this.tiltAngle = 0;
+}
+
+function draw(canvas){
+	var ctx = canvas.getContext("2d");
+
+	//Draw joystick base
+	ctx.beginPath();
+	ctx.arc(this.centerX, this.centerY, this.radius, 0, 2 * Math.PI, false);
+	ctx.fillStyle = "#FF0000";
+	ctx.fill();
+	ctx.strokeStyle = "#003300";
+	ctx.stroke();
+
+	//Draw joystick head
+	ctx.beginPath();
+	ctx.arc(this.centerX + (this.tiltMagnitude * Math.cos(this.tiltAngle)), this.centerY + (this.tiltMagnitude * Math.sin(this.tiltAngle)), this.radius * 0.8, 0, 2 * Math.PI, false);
+	ctx.fillStyle = "#0000FF";
+	ctx.fill();
+	ctx.strokeStyle = "#001100";
+	ctx.stroke();
 }
--- phone.js
+++ phone.js
@@ -55,10 +55,12 @@ function webGLStart() {
 	document.onkeypress = handleKeyPress;
 	*/
 
+	/*
 	//Set touch functions
 	canvas.addEventListener("touchstart", handleTouchStart, false);
 	canvas.addEventListener("touchmove", handleTouchMove, false);
 	canvas.addEventListener("touchend", handleTouchEnd, false);
+	*/
 
 	try {
 		sphere.loadBuffers(gl);
@@ -72,285 +74,31 @@ function webGLStart() {
 	var h = canvasHeight;
 
 	//Handle connection
-	connection = new WebSocket('ws://peaceful-lake-7978.herokuapp.com/');
-	connection.onmessage = receiveMessage;
+	//connection = new WebSocket('ws://peaceful-lake-7978.herokuapp.com/');
+	//connection.onmessage = receiveMessage;
 
 	// start the drawing loop
 	tick();
 };
 
-function lookAt(eye, at, up) {
-
-	var uz = eye.minus(at);
-
-	uz = uz.times(1 / uz.length());
-
-	var ux = up.cross(uz);
-
-	ux = ux.times(1 / ux.length());
-
-	var uy = uz.cross(ux);
-
-	var R = new Mat(ux, uy, uz);
-
-	var T = Mat.transMatrix(eye);
-
-	view2world = T.times(R);
-
-	var Rinv = R.transpose();
-
-	var Tinv = Mat.transMatrix(eye.minus());
-
-	world2view = Rinv.times(Tinv);
-};
-
-function setPerspective(far) {
-
-	var a = -(far + 1) / (far - 1);
-	var b = a - 1;
-
-	view2clip = new Mat();
-	view2clip.set(2, 2, a);
-	view2clip.set(2, 3, b);
-	view2clip.set(3, 2, -1);
-	view2clip.set(3, 3, 0);
-
-	view2clip = view2clip.times(Mat.scaleMatrix(new PV(canvasHeight
-			/ canvasWidth, 1, 1, true)));
-
-	clip2view = new Mat();
-	clip2view.set(2, 2, 0);
-	clip2view.set(2, 3, -1);
-	clip2view.set(3, 2, 1 / b);
-	clip2view.set(3, 3, a / b);
-
-	clip2view = Mat.scaleMatrix(new PV(canvasWidth / canvasHeight, 1, 1, true))
-			.times(clip2view);
-
-	updateMat();
-};
-
-function updateMat() {
-	model2clip = view2clip.times(world2view).times(tran).times(rot);
-};
-
 function render() {
 
+	//Clear the screen
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-	gl.uniformMatrix4fv(shader.getUniform(gl, "mat"), false, new Float32Array(
-			model2clip.getBuffer()));
+	//Draw the joysticks
+	var canvas = document.getElementById("canvas");
+	joystick1.draw(canvas);
+	joystick2.draw(canvas);
 
-	var lightP = rotInv.times(tranInv.times(new PV(-10, 0, 10, true)));
-	var lightI = new PV(1, 1, 1, true);
-	lightI.set(3, 0.9);
-	var eyeP = rotInv.times(tranInv.times(view2world.times(new PV(true))));
+	//Send info
 
-	gl.uniform4fv(shader.getUniform(gl, "lightP"), lightP.array);
-	gl.uniform4fv(shader.getUniform(gl, "lightI"), lightI.array);
-	gl.uniform4fv(shader.getUniform(gl, "eyeP"), eyeP.array);
-
-	sphere.draw(gl, shader, "vs_position", "vs_color", "vs_normal");
 };
 
 var mouseDown = false;
 var windowPress;
 var vMin;
 
-//Handles what happens when the canvas is touched
-function handleTouchStart(event){
-	event.preventDefault();
-	var touches = event.changedTouches;
-
-	//Process max 2 touches at a time (one for each joystick)
-	if(touches.length >= 2)
-		return;
-	
-}
-
-function handleTouchMove(event){
-
-}
-
-function handleTouchEnd(event){
-
-}
-
-function handleMouseDown(event) {
-	mouseDown = true;
-
-	var pixelDistance = 4;
-
-	windowPress = client2canvas.times(new PV(event.clientX, event.clientY, 0,
-			true));
-
-	vMin = null;
-	var dMin = 1e9;
-	for ( var i = 0; i < sphere.verts.length; i++) {
-		var v = sphere.verts[i];
-
-		var windowV = clip2window.times(model2clip.times(v.p));
-		// ...and set its z to zero because that's the z of windowPress.
-		windowV.set(2, 0.0);
-
-		var d = windowPress.distance(windowV);
-		if (d <= dMin) {
-			dMin = d;
-			vMin = v;
-		}
-	}
-
-	if (dMin > pixelDistance)
-		vMin = null;
-
-}
-
-function handleMouseUp(event) {
-	mouseDown = false;
-}
-
-function handleMouseMove(event) {
-	if (!mouseDown) {
-		return;
-	}
-
-	var windowDrag = client2canvas.times(new PV(event.clientX, event.clientY,
-			0, true));
-	if (vMin == null) {
-
-		var clipPress = window2clip.times(windowPress);
-
-		var clipDrag = window2clip.times(windowDrag);
-
-		var clipO = model2clip.times(new PV(true));
-
-		clipPress.set(2, clipO.get(2));
-		clipDrag.set(2, clipO.get(2));
-
-		var worldPress = view2world.times(clip2view.times(clipPress));
-
-		var worldDrag = view2world.times(clip2view.times(clipDrag));
-
-		var t = worldDrag.minus(worldPress);
-		
-		var tr = Mat.transMatrix(t).times(tran);
-		var trInv = tranInv.times(Mat.transMatrix(t.minus()));
-		
-		translate(tr, trInv);
-		
-		var s = "trans";
-		
-		for(var i = 0; i < tr.array.length; i++) {
-			s += " " + tr.array[i];
-		}
-		
-		for(var i = 0; i < trInv.array.length; i++) {
-			s += " " + trInv.array[i];
-		}
-
-		connection.send(s);
-		
-	} else {
-
-		var o = new PV(true);
-
-		var v = rot.times(vMin.p.minus(o));
-
-		var clipDragFront = window2clip.times(windowDrag);
-		clipDragFront.set(2, -1.0);
-
-		clipDragBack = window2clip.times(windowDrag);
-		clipDragBack.set(2, 1.0);
-
-		var wFront = tranInv.times(view2world.times(clip2view
-				.times(clipDragFront)));
-
-		var wBack = tranInv.times(view2world.times(clip2view
-				.times(clipDragBack)));
-
-		var of = wFront.minus(o);
-
-		var fb = wBack.minus(wFront);
-		fb = fb.times(1 / fb.length());
-
-		var s = -of.dot(fb);
-
-		var w = of.plus(fb.times(s));
-
-		var r = v.length();
-		var l = w.length();
-
-		if (l <= r) {
-
-			var z = Math.sqrt(r * r - l * l);
-
-			if (v.dot(fb) > 0)
-				w = w.plus(fb.times(z));
-			else
-				w = w.minus(fb.times(z));
-		} else {
-			w.times(r / l);
-		}
-
-		var vx = v;
-		vx = vx.times(1 / vx.length());
-
-		var wx = w;
-		wx = wx.times(1 / wx.length());
-
-		var vz = vx.cross(wx);
-
-		if (vz.length() < 1e-3)
-			return;
-
-		vz = vz.times(1 / vz.length());
-
-		var wz = vz;
-
-		var vy = vz.cross(vx);
-
-		var wy = wz.cross(wx);
-
-		var vMat = new Mat(vx, vy, vz);
-
-		var wMat = new Mat(wx, wy, wz);
-
-		var vwMat = wMat.times(vMat.transpose());
-
-		var vwMatInv = vwMat.transpose();
-		
-		var r = vwMat.times(rot);
-		var rInv = rotInv.times(vwMatInv);
-		
-		rotate(r, rInv);
-		
-		var s = "rot";
-		
-		for(var i = 0; i < r.array.length; i++) {
-			s += " " + r.array[i];
-		}
-		
-		for(var i = 0; i < rInv.array.length; i++) {
-			s += " " + rInv.array[i];
-		}
-		
-		connection.send(s);
-	}
-	windowPress = windowDrag;
-}
-
-
-function translate(t, tInv) {
-	tran = t;
-	tranInv = tInv;
-	updateMat();
-}
-
-function rotate(r, rInv) {
-	rot = r;
-	rotInv = rInv;
-	updateMat();
-}
 
 function receiveMessage(e) {
 
@@ -364,7 +112,8 @@ function receiveMessage(e) {
 //	});
 //	
 //	reader.readAsArrayBuffer(e.data);
-	
+
+	/*
 	var a = e.data.split(' ');
 		
 	var m = new Mat();
@@ -383,17 +132,5 @@ function receiveMessage(e) {
 	} else if(a[0] === 'rot') {
 		rotate(m, mInv);
 	}
-
-}
-
-function move(a) {
-	if(a.length == 4) {
-		var t = new PV(false);
-		t.array = a;
-		translate(t);
-	} else if(a.length == 16) {
-		var m = new Mat();
-		m.array = a;
-		rotate(m);
-	}
+	*/
 }
