e22d2176739d70ab60c4f88a6c22dc8a3a1d05f1
Chloe Arluck <chloearluck@gmail.com>
2017-12-02 23:15:49 -0500

    2 ships and implement king of the hilll

--- sketch.js
+++ sketch.js
@@ -3,9 +3,15 @@
 
 var bullets;
 var asteroids;
-var ship;
+var redShips;
+var blueShips;
+var domain;
 var shipImage, bulletImage, particleImage;
 var MARGIN = 40;
+var domainRadius = 200;
+var initialTime;
+var domainChangeInterval = 5000;
+var timeLastDomainChange;
 
 function setup() {
     createCanvas(1400,800);
@@ -14,16 +20,17 @@ function setup() {
     shipImage = loadImage("assets/ship_rot.png");
     particleImage = loadImage("assets/asteroids_particle.png");
 
-    ship = createSprite(width/2, height/2);
-    ship.maxSpeed = 6;
-    ship.friction = .1;
-    ship.setCollider("circle", 0,0, 20);
+    redShips = [createShip()];
+    blueShips = [createShip()];
 
-    ship.addImage(shipImage);
     //ship.addAnimation("thrust", "assets/ship_20.png", "assets/ship_20.png");
 
     asteroids = new Group();
     bullets = new Group();
+    domain = {x: Math.random()*width, y: Math.random()*height};
+
+    initialTime = new Date();
+    timeLastDomainChange = initialTime;
 
     for(var i = 0; i<8; i++) {
 	var ang = random(360);
@@ -31,6 +38,8 @@ function setup() {
 	var py = height/2+ 1000 * sin(radians(ang));
 	createAsteroid(3, px, py);
     }
+
+
 }
 
 function draw() {
@@ -51,33 +60,63 @@ function draw() {
 	if(s.position.y<-MARGIN) s.position.y = height+MARGIN;
 	if(s.position.y>height+MARGIN) s.position.y = -MARGIN;
     }
-  
+
+    var currentTime = new Date();
+    if (currentTime.getTime() - timeLastDomainChange.getTime() > domainChangeInterval) {
+        timeLastDomainChange = currentTime;
+        domain = {x: Math.random()*width, y: Math.random()*height};
+    }
+
     asteroids.overlap(bullets, asteroidHit);
   
-    ship.bounce(asteroids);
+    redShips[0].bounce(asteroids);
   
     if(keyDown(LEFT_ARROW))
-	ship.rotation -= 4;
+    redShips[0].rotation -= 4;
     if(keyDown(RIGHT_ARROW))
-	ship.rotation += 4;
+    redShips[0].rotation += 4;
     if(keyDown(UP_ARROW))
-	{
-	    ship.addSpeed(2.9, ship.rotation);
-	    //ship.changeAnimation("thrust");
-	}
+    {
+        redShips[0].addSpeed(2.9, redShips[0].rotation);
+        //ship.changeAnimation("thrust");
+    }
     //else
-	//ship.changeAnimation("normal");
+    //ship.changeAnimation("normal");
+    
+    if(keyDown("a"))
+    blueShips[0].rotation -= 4;
+    if(keyDown("d"))
+    blueShips[0].rotation += 4;
+    if(keyDown("s"))
+    {
+        blueShips[0].addSpeed(2.9, blueShips[0].rotation);
+        //ship.changeAnimation("thrust");
+    }
+    //else
+    //ship.changeAnimation("normal");
     
     if(keyWentDown("x"))
-	{
-	    var bullet = createSprite(ship.position.x, ship.position.y);
-	    bullet.addImage(bulletImage);
-	    bullet.setSpeed(10+ship.getSpeed(), ship.rotation);
-	    bullet.rotation = ship.rotation;
-	    bullet.life = 60;
-	    bullets.add(bullet);
-	}
+    {
+        var bullet = createSprite(redShips[0].position.x, redShips[0].position.y);
+        bullet.addImage(bulletImage);
+        bullet.setSpeed(10+redShips[0].getSpeed(), redShips[0].rotation);
+        bullet.rotation = redShips[0].rotation;
+        bullet.life = 60;
+        bullets.add(bullet);
+    }
+
+    if(keyWentDown("w"))
+    {
+        var bullet = createSprite(blueShips[0].position.x, blueShips[0].position.y);
+        bullet.addImage(bulletImage);
+        bullet.setSpeed(10+blueShips[0].getSpeed(), blueShips[0].rotation);
+        bullet.rotation = blueShips[0].rotation;
+        bullet.life = 60;
+        bullets.add(bullet);
+    }
+
   
+    drawDomain();
     drawSprites();
   
 }
@@ -102,6 +141,15 @@ function createAsteroid(type, x, y) {
     return a;
 }
 
+function createShip() {
+    var ship = createSprite(width/2, height/2);
+    ship.maxSpeed = 6;
+    ship.friction = .1;
+    ship.setCollider("circle", 0,0, 20);
+    ship.addImage(shipImage);
+    return ship; 
+}
+
 function asteroidHit(asteroid, bullet) {
     var newType = asteroid.type-1;
 
@@ -120,4 +168,33 @@ function asteroidHit(asteroid, bullet) {
 
     bullet.remove();
     asteroid.remove();
+}
+
+function changeDomainPos() {
+    domain = {x: Math.random()*width, y: Math.random()*height};
+}
+
+function drawDomain() {
+    var redCount = 0;
+    var blueCount = 0;
+    
+    // console.log(redShips[0].position.x + " " + redShips[0].position.y);
+    // console.log(blueShips[0].position.x + " " + blueShips[0].position.y);
+
+    for (var i=0; i<redShips.length; i++) 
+        if (Math.sqrt(Math.pow(redShips[i].position.x-domain.x, 2) + Math.pow(redShips[i].position.y-domain.y, 2)) <domainRadius)
+            redCount++;
+    for (var i=0; i<blueShips.length; i++) 
+        if (Math.sqrt(Math.pow(blueShips[i].position.x-domain.x, 2) + Math.pow(blueShips[i].position.y-domain.y, 2)) <domainRadius)
+            blueCount++;
+
+    if (redCount == 0 && blueCount > 0)
+        fill(0, 0, 255, 100);
+    else if (blueCount == 0 && redCount > 0)
+        fill(255, 0, 0, 100);
+    else
+        fill(125, 125, 125, 100);
+
+    noStroke();
+    ellipse(domain.x, domain.y, 2*domainRadius, 2*domainRadius);
 }
